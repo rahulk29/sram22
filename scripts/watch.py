00df8cac5f2a28e342e1ccd9c9e5b760e3565fb3
rahulk29 <rahulkumar@berkeley.edu>
2022-02-15 18:35:11 -0800

    update docs; add Python script to generate images (#17)

--- scripts/watch.py
+++ scripts/watch.py
@@ -8,7 +8,7 @@ out_dir = "_build/"
 cell = "sram_8x8.mag"
 outsvg = "out.svg"
 
-script = f'''
+script = f"""
 magic -T sky130A -d XR -noconsole <<EOF
 load {cell}
 select top cell
@@ -18,10 +18,10 @@ select clear
 plot svg {outsvg}
 quit -noprompt
 EOF
-'''
+"""
 
 os.system(command)
 os.chdir(out_dir)
 cwd = os.getcwd()
 os.system(script)
-webbrowser.open(f'file://{cwd}/{outsvg}')
+webbrowser.open(f"file://{cwd}/{outsvg}")
