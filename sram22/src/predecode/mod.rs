515ac127090b9b65d4df5c1621c443eb66d703f8
Rahul Kumar <rahulkumar@berkeley.edu>
2022-02-16 15:37:23 -0800

    generate simple 2-4 predecoder

--- sram22/src/predecode/mod.rs
+++ sram22/src/predecode/mod.rs
@@ -1,7 +1,12 @@
+use magic_vlsi::units::{Distance, Vec2};
+use magic_vlsi::{Direction, MagicInstance};
 use micro_hdl::{context::Context, node::Node};
 
 use crate::cells::gates::inv::Inv;
 use crate::cells::gates::nand3::Nand3;
+use crate::config::TechConfig;
+use crate::error::Result;
+use crate::layout::bus::BusBuilder;
 
 #[micro_hdl::module]
 pub struct Predecoder38 {
@@ -64,6 +69,49 @@ impl Predecoder38 {
     fn name() -> String {
         "predecoder3_8".to_string()
     }
+}
+
+pub fn generate_predecoder2_4(m: &mut MagicInstance, tc: &TechConfig) -> Result<()> {
+    let nand2_pm_sh = m.load_layout_cell("nand2_pm_sh")?;
+    let inv_pm_sh = m.load_layout_cell("inv_pm_sh_2")?;
+
+    let cell_name = String::from("predecoder2_4");
+    m.drc_off()?;
+    m.load(&cell_name)?;
+    m.enable_box()?;
+    m.set_snap(magic_vlsi::SnapMode::Internal)?;
+
+    let mut height = Distance::zero();
+    let mut nands = Vec::with_capacity(4);
+    for _ in 0..4 {
+        let nand2 =
+            m.place_layout_cell(nand2_pm_sh.clone(), Vec2::new(Distance::zero(), height))?;
+        m.place_layout_cell(inv_pm_sh.clone(), nand2.bbox().lr())?;
+        height = nand2.bbox().top_edge();
+        nands.push(nand2);
+    }
+
+    let bus = BusBuilder::new()
+        .width(4)
+        .dir(Direction::Up)
+        .tech_layer(tc, "m1")
+        .allow_contact(tc, "ct", "li")
+        .allow_contact(tc, "via1", "m2")
+        .align_right(Distance::zero())
+        .start(Distance::zero())
+        .end(height)
+        .draw(m)?;
+
+    for (i, gate) in nands.iter().enumerate() {
+        let target = gate.port_bbox("A");
+        bus.draw_contact(m, tc, 1 - (i % 2), "ct", "viali", "li", target)?;
+        let target = gate.port_bbox("B");
+        bus.draw_contact(m, tc, 3 - (i / 2), "ct", "viali", "li", target)?;
+    }
+
+    m.save(&cell_name)?;
+
+    Ok(())
 }
 
 #[cfg(test)]
