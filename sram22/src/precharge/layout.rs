b3ddbee2093e6e360f8d82a8a1ad305dbf0be084
Rahul Kumar <rahulkumar@berkeley.edu>
2022-03-09 11:40:38 -0800

    empty shell for layout

--- sram22/src/precharge/layout.rs
+++ sram22/src/precharge/layout.rs
@@ -1,1 +1,14 @@
+use crate::error::Result;
+use magic_vlsi::MagicInstance;
 
+use crate::config::TechConfig;
+
+use super::PrechargeSize;
+
+pub fn generate_precharge(
+    _m: &mut MagicInstance,
+    _tc: &TechConfig,
+    _params: PrechargeSize,
+) -> Result<()> {
+    unimplemented!()
+}
