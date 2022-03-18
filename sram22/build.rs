6d876c03a7f90c23213216dda54999d399e776da
Rahul Kumar <rahulkumar@berkeley.edu>
2022-03-18 14:29:57 -0700

    make format

--- sram22/build.rs
+++ sram22/build.rs
@@ -1,6 +1,9 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["../protos/sram22/verification/sim/v1/sim.proto"], &["../protos/"])?;
+    prost_build::compile_protos(
+        &["../protos/sram22/verification/sim/v1/sim.proto"],
+        &["../protos/"],
+    )?;
     Ok(())
 }
