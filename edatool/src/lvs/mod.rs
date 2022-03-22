22ccba7c939f69a12b134611eb9af7444f19537a
Rahul Kumar <rahulkumar@berkeley.edu>
2022-03-22 15:59:41 -0700

    update edatool LVS protos

--- edatool/src/lvs/mod.rs
+++ edatool/src/lvs/mod.rs
@@ -1,26 +1,10 @@
-use crate::error::Result;
 use std::path::PathBuf;
 
-pub struct LvsInput<S> {
-    pub netlist: PathBuf,
-    pub layout: PathBuf,
-    pub netlist_cell: String,
-    pub layout_cell: String,
-    pub work_dir: PathBuf,
-    pub opts: S,
-}
+use crate::{
+    error::Result,
+    protos::lvs::{LvsInput, LvsOutput},
+};
 
-pub struct LvsOutput<E> {
-    pub ok: bool,
-    pub errors: Vec<E>,
-}
-
-pub trait Lvs<S, E> {
-    fn lvs(&self, input: LvsInput<S>) -> Result<LvsOutput<E>>;
-}
-
-pub struct LvsOpts {}
-
-pub struct LvsError {
-    pub msg: String,
+pub trait Lvs {
+    fn lvs(&self, input: LvsInput, work_dir: PathBuf) -> Result<LvsOutput>;
 }
