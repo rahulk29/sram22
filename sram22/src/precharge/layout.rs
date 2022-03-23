a91144a1d459dc75646510b2b90d4db77538f890
Rahul Kumar <rahulkumar@berkeley.edu>
2022-03-22 22:24:54 -0700

    update precharge generator with poly gate contact and ports

--- sram22/src/precharge/layout.rs
+++ sram22/src/precharge/layout.rs
@@ -3,6 +3,7 @@ use crate::error::Result;
 use crate::factory::Component;
 use crate::layout::{draw_contact, draw_contacts, ContactStack};
 use magic_vlsi::units::Rect;
+use magic_vlsi::Direction;
 use magic_vlsi::{units::Distance, MagicInstance};
 
 use crate::config::TechConfig;
@@ -92,17 +93,38 @@ pub fn generate_precharge(
     nwell_box.ur.x = width;
     m.paint_box(nwell_box, "nwell")?;
 
+    let poly_box_extension = [
+        tc.layer("poly").extension("pdiff"),
+        tc.layer("li").space,
+        tc.space("licon", "pdiff") - tc.layer("licon").enclosure("poly"),
+    ]
+    .into_iter()
+    .max()
+    .unwrap();
     let poly_height = pmos_pass_width
         + 2 * pmos_width
         + 2 * tc.layer("pdiff").space
-        + 2 * tc.layer("poly").extension("pdiff");
+        + tc.layer("poly").extension("pdiff")
+        + poly_box_extension;
     let poly_box = Rect::ll_wh(
         pdiff_box1.left_edge() + pdiff_edge_to_gate(tc),
-        pdiff_box1.bottom_edge() - tc.layer("poly").extension("pdiff"),
+        pdiff_box1.bottom_edge() - poly_box_extension,
         tc.layer("poly").width,
         poly_height,
     );
     m.paint_box(poly_box, "poly")?;
+
+    let poly_pad_h = tc.layer("licon").width + 2 * tc.layer("licon").enclosure("poly");
+    let poly_pad_w = tc.layer("licon").width + 2 * tc.layer("licon").one_side_enclosure("poly");
+    let poly_pad_box = Rect::ul_wh(
+        Distance::zero(),
+        poly_box.bottom_edge(),
+        poly_pad_w,
+        poly_pad_h,
+    );
+    let poly_pad_box = poly_pad_box.try_align_center_x(poly_box, tc.grid);
+    m.paint_box(poly_pad_box, "poly")?;
+    m.paint_box(poly_pad_box, "li")?;
 
     let bl_right = poly_box.left_edge() - tc.space("gate", "licon");
     let bl_box = Rect::from_dist(
@@ -120,18 +142,39 @@ pub fn generate_precharge(
         pdiff_box3.top_edge(),
     );
 
+    let polyc_stack = ContactStack {
+        top: "li",
+        contact_drc: "licon",
+        contact_layer: "polyc",
+        bot: "poly",
+    };
+
     let viali_stack = ContactStack {
         top: "m1",
         contact_drc: "ct",
         contact_layer: "viali",
         bot: "li",
     };
+
     let via1_stack = ContactStack {
         top: "m2",
         contact_drc: "via1",
         contact_layer: "via1",
         bot: "m1",
     };
+
+    let _gate_ct = draw_contact(m, tc, polyc_stack, poly_pad_box, false)?;
+    let gate_ct = draw_contact(m, tc, via1_stack, poly_pad_box, false)?;
+
+    let m2_box = Rect::from_dist(
+        Distance::zero(),
+        gate_ct.top.bottom_edge(),
+        width,
+        gate_ct.top.top_edge(),
+    );
+    m.paint_box(m2_box, "m2")?;
+    m.label_position_layer("PC_EN_BAR", Direction::Left, "m2")?;
+    m.port_make_default()?;
 
     let bl_pass_ct = draw_contact(m, tc, viali_stack, bl_box.overlap(pdiff_box1), true)?;
     let blb_pass_ct = draw_contact(m, tc, viali_stack, blb_box.overlap(pdiff_box1), true)?;
@@ -147,6 +190,9 @@ pub fn generate_precharge(
     )
     .try_align_center_x(bl_pass_ct.top, tc.grid);
     m.paint_box(bl_m1_box, "m1")?;
+    m.label_position_layer("BL", Direction::Up, "m1")?;
+    m.port_make_default()?;
+
     let blb_m1_box = Rect::btcxw(
         blb_pass_ct.top.bottom_edge(),
         nwell_box.top_edge(),
@@ -155,6 +201,8 @@ pub fn generate_precharge(
     )
     .try_align_center_x(blb_pass_ct.top, tc.grid);
     m.paint_box(blb_m1_box, "m1")?;
+    m.label_position_layer("BL_BAR", Direction::Up, "m1")?;
+    m.port_make_default()?;
 
     for li_box in [bl_box, blb_box].into_iter() {
         for diff_box in [pdiff_box1, pdiff_box2, pdiff_box3].into_iter() {
@@ -187,6 +235,8 @@ pub fn generate_precharge(
         ct2.top.top_edge(),
     );
     m.paint_box(m2_box, "m2")?;
+    m.label_position_layer("VPWR1", Direction::Right, "m2")?;
+    m.port_make_default()?;
 
     let blb_vdd_ct = Rect::ll_wh(
         Distance::zero(),
@@ -210,6 +260,8 @@ pub fn generate_precharge(
         ct2.top.top_edge(),
     );
     m.paint_box(m2_box, "m2")?;
+    m.label_position_layer("VPWR2", Direction::Left, "m2")?;
+    m.port_make_default()?;
 
     // prune overhangs
     let erase_box = Rect::ll_wh(
