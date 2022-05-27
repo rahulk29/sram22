b22f2a85d00876c1e006b22d43ee6f5a2f696750
rahulk29 <rahulkumar@berkeley.edu>
2022-05-26 21:11:49 -0700

    add open ram sims, begin col mux layout generation (#82)

--- sramgen/src/layout/mux.rs
+++ sramgen/src/layout/mux.rs
@@ -11,7 +11,7 @@ use pdkprims::{
 use crate::{bbox, Result};
 
 fn draw_mux(lib: &mut PdkLib) -> Result<Ptr<Cell>> {
-    let name = "precharge".to_string();
+    let name = "read_mux".to_string();
 
     let mut layout = Layout {
         name: name.clone(),
@@ -49,9 +49,8 @@ fn draw_mux(lib: &mut PdkLib) -> Result<Ptr<Cell>> {
         angle: Some(90f64),
         reflect_vert: false,
     };
-    let bbox = bbox(&ptx.cell);
-    assert_eq!(bbox.height(), 1040);
     let bbox = mos.bbox();
+    assert_eq!(bbox.width(), 1040);
     layout.insts.push(mos);
 
     let tc = lib.pdk.config();
@@ -61,17 +60,68 @@ fn draw_mux(lib: &mut PdkLib) -> Result<Ptr<Cell>> {
     println!("bbox width: {}", bbox.width());
 
     layout.insts.push(Instance {
+        inst_name: "mos_2".to_string(),
+        cell: ptx.cell.clone(),
+        loc: Point::new(bbox.width() + space, 0),
+        angle: Some(90f64),
+        reflect_vert: false,
+    });
+
+    let cell = Cell {
+        name,
+        abs: None,
+        layout: Some(layout),
+    };
+
+    let ptr = Ptr::new(cell);
+    lib.lib.cells.push(ptr.clone());
+
+    Ok(ptr)
+}
+
+fn draw_write_mux(lib: &mut PdkLib) -> Result<Ptr<Cell>> {
+    let name = "write_mux".to_string();
+
+    let mut layout = Layout {
+        name: name.clone(),
+        insts: vec![],
+        elems: vec![],
+        annotations: vec![],
+    };
+
+    let mut params = MosParams::new();
+    params
+        .dnw(false)
+        .direction(CoarseDirection::Horizontal)
+        .add_device(MosDevice {
+            mos_type: MosType::Nmos,
+            width: 1_200,
+            length: 150,
+            fingers: 1,
+            intent: Intent::Svt,
+            skip_sd_metal: vec![],
+        });
+    let ptx = lib.draw_mos(params)?;
+
+    let mos = Instance {
         inst_name: "mos_1".to_string(),
         cell: ptx.cell.clone(),
         loc: Point::new(0, 0),
         angle: Some(90f64),
         reflect_vert: false,
-    });
+    };
+    let bbox = mos.bbox();
+    layout.insts.push(mos);
+
+    let tc = lib.pdk.config();
+    let tc = tc.read().unwrap();
+
+    let space = tc.layer("diff").space;
 
     layout.insts.push(Instance {
         inst_name: "mos_2".to_string(),
         cell: ptx.cell.clone(),
-        loc: Point::new(bbox.width(), 0),
+        loc: Point::new(0, bbox.height() + space),
         angle: Some(90f64),
         reflect_vert: false,
     });
@@ -98,6 +148,16 @@ mod tests {
     fn test_sky130_column_mux() -> Result<()> {
         let mut lib = sky130::pdk_lib("test_sky130_column_mux")?;
         draw_mux(&mut lib)?;
+
+        lib.save_gds()?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_sky130_column_write_mux() -> Result<()> {
+        let mut lib = sky130::pdk_lib("test_sky130_column_write_mux")?;
+        draw_write_mux(&mut lib)?;
 
         lib.save_gds()?;
 
