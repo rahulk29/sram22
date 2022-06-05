d85f69bf9c0962e1969f2b9b691890534fd54dcb
Rahul Kumar <rahulkumar@berkeley.edu>
2022-06-05 14:13:52 -0700

    correct cell placement in sram array

--- sramgen/src/layout/array.rs
+++ sramgen/src/layout/array.rs
@@ -20,22 +20,14 @@ pub fn draw_array(rows: usize, cols: usize, lib: &mut PdkLib) -> Result<Ptr<Cell
 
     let corner = corner_gds(lib)?;
     let colend_cent = colend_cent_gds(lib)?;
-    let colend_cent_bbox = bbox(&colend_cent);
+    let colend_p_cent = colend_p_cent_gds(lib)?;
     let colend = colend_gds(lib)?;
     let colend_bbox = bbox(&colend);
 
     let bitcell = sram_sp_cell_gds(lib)?;
-    let bitcell_bbox = bbox(&bitcell);
     let rowend = rowend_gds(lib)?;
-    let rowend_bbox = bbox(&rowend);
-    let rowend_width = 1300;
-    let row_height = 1580;
-    let colend_width = 1200;
-    let colend_height = 2055;
     let wlstrap = wlstrap_gds(lib)?;
-    let wlstrap_bbox = bbox(&wlstrap);
     let wlstrap_p = wlstrap_p_gds(lib)?;
-    let wlstrap_p_bbox = bbox(&wlstrap_p);
     assert_eq!(colend_bbox.width(), 1200);
 
     let mut grid = GridCells::new();
@@ -58,9 +50,15 @@ pub fn draw_array(rows: usize, cols: usize, lib: &mut PdkLib) -> Result<Ptr<Cell
     });
 
     for i in 1..cols {
+        let colend_cent_i = if i % 2 == 0 {
+            colend_cent.clone()
+        } else {
+            colend_p_cent.clone()
+        };
+
         row.push(Instance {
             inst_name: format!("colend_cent_top_{}", i),
-            cell: colend_cent.clone(),
+            cell: colend_cent_i,
             loc: Point::new(0, 0),
             reflect_vert: false,
             angle: None,
@@ -69,8 +67,8 @@ pub fn draw_array(rows: usize, cols: usize, lib: &mut PdkLib) -> Result<Ptr<Cell
             inst_name: format!("colend_top_{}", i),
             cell: colend.clone(),
             loc: Point::new(0, 0),
-            reflect_vert: false,
-            angle: None,
+            reflect_vert: i % 2 != 0,
+            angle: if i % 2 != 0 { Some(180f64) } else { None },
         });
     }
 
@@ -113,16 +111,24 @@ pub fn draw_array(rows: usize, cols: usize, lib: &mut PdkLib) -> Result<Ptr<Cell
                 inst_name: format!("wlstrap_{}_{}", r, c),
                 cell: strap,
                 loc: Point::new(0, 0),
-                reflect_vert: false,
+                reflect_vert: r % 2 == 0,
                 angle: None,
             });
+
+            let (reflect_vert, angle) = match (r % 2, c % 2) {
+                (0, 0) => (false, Some(180f64)),
+                (0, 1) => (true, None),
+                (1, 0) => (true, Some(180f64)),
+                (1, 1) => (false, None),
+                _ => unreachable!("invalid mods"),
+            };
 
             row.push(Instance {
                 inst_name: format!("cell_{}_{}", r, c),
                 cell: bitcell.clone(),
                 loc: Point::new(0, 0),
-                reflect_vert: false,
-                angle: None,
+                reflect_vert,
+                angle,
             });
         }
 
@@ -130,7 +136,7 @@ pub fn draw_array(rows: usize, cols: usize, lib: &mut PdkLib) -> Result<Ptr<Cell
             inst_name: format!("rowend_r_{}", r),
             cell: rowend.clone(),
             loc: Point::new(0, 0),
-            reflect_vert: false,
+            reflect_vert: r % 2 == 0,
             angle: None,
         });
 
@@ -144,23 +150,29 @@ pub fn draw_array(rows: usize, cols: usize, lib: &mut PdkLib) -> Result<Ptr<Cell
         cell: corner.clone(),
         loc: Point::new(0, 0),
         reflect_vert: false,
-        angle: None,
+        angle: Some(180f64),
     });
 
     row.push(Instance {
         inst_name: "colend_bot_0".to_string(),
         cell: colend.clone(),
         loc: Point::new(0, 0),
-        reflect_vert: false,
+        reflect_vert: true,
         angle: None,
     });
 
     for i in 1..cols {
+        let colend_cent_i = if i % 2 == 0 {
+            colend_cent.clone()
+        } else {
+            colend_p_cent.clone()
+        };
+
         row.push(Instance {
             inst_name: format!("colend_cent_bot_{}", i),
-            cell: colend_cent.clone(),
+            cell: colend_cent_i,
             loc: Point::new(0, 0),
-            reflect_vert: false,
+            reflect_vert: true,
             angle: None,
         });
 
@@ -168,8 +180,8 @@ pub fn draw_array(rows: usize, cols: usize, lib: &mut PdkLib) -> Result<Ptr<Cell
             inst_name: format!("colend_bot_{}", i),
             cell: colend.clone(),
             loc: Point::new(0, 0),
-            reflect_vert: false,
-            angle: None,
+            reflect_vert: i % 2 == 0,
+            angle: if i % 2 != 0 { Some(180f64) } else { None },
         });
     }
 
@@ -177,7 +189,7 @@ pub fn draw_array(rows: usize, cols: usize, lib: &mut PdkLib) -> Result<Ptr<Cell
         inst_name: "corner_br".to_string(),
         cell: corner.clone(),
         loc: Point::new(0, 0),
-        reflect_vert: false,
+        reflect_vert: true,
         angle: None,
     });
 
