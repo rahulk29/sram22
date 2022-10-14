d68f558a94e595b5f8cb082ed603fe2c655634d3
rahulk29 <rahulkumar@berkeley.edu>
2022-10-13 22:22:47 -0700

    Fix SPICE transistor header for SKY130 (#166)

--- sramgen/scripts/generate.py
+++ sramgen/scripts/generate.py
@@ -27,11 +27,11 @@ CKTS = [
 SPECTRE_PRELUDE = """* SPICE NETLIST
 * * NMOS
 .SUBCKT sky130_fd_pr__nfet_01v8 d g s b PARAMS: w=1.0 l=1.0
-M0 d g s b nshort l={l} w={w}
+M0 d g s b nshort l='l' w='w'
 .ENDS
 * * PMOS
 .SUBCKT sky130_fd_pr__pfet_01v8 d g s b PARAMS: w=1.0 l=1.0
-M0 d g s b pshort l={l} w={w}
+M0 d g s b pshort l='l' w='w'
 .ENDS
 """
 
