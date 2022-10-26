52d46668bf889320abf511f80afd7eaf214af24b
rahulk29 <rahulkumar@berkeley.edu>
2022-10-26 12:36:48 -0700

    Update control logic; add buffer standard cell (#204)

--- sramgen/src/layout/control.rs
+++ sramgen/src/layout/control.rs
@@ -1,6 +1,6 @@
 use crate::layout::bank::GateList;
 use crate::layout::common::MergeArgs;
-use crate::tech::{sc_and2_gds, sc_inv_gds, sc_tap_gds};
+use crate::tech::{sc_and2_gds, sc_inv_gds, sc_tap_gds, sc_buf_gds};
 use crate::Result;
 
 use layout21::raw::{Cell, Instance};
@@ -23,7 +23,6 @@ pub struct InvChainParams<'a> {
 }
 
 pub fn draw_inv_chain(lib: &mut PdkLib, params: InvChainParams) -> Result<Ptr<Cell>> {
-    assert_eq!(params.num % 2, 0);
     let mut cell = Cell::empty(params.prefix);
 
     let inv = sc_inv_gds(lib)?;
@@ -102,20 +101,65 @@ pub fn draw_control_logic(lib: &mut PdkLib, mode: ControlMode) -> Result<Ptr<Cel
     let mut cell = Cell::empty("sram22_control_logic");
 
     let and = sc_and2_gds(lib)?;
+    let inv = sc_inv_gds(lib)?;
+    let buf = sc_buf_gds(lib)?;
     let tap = sc_tap_gds(lib)?;
+    let delay_chain = draw_inv_chain(lib, InvChainParams { prefix: "sram22_control_logic_delay_chain", num: 25 })?;
 
     let tap0 = Instance::new("tap0", tap.clone());
     let mut tap1 = Instance::new("tap1", tap);
+    let mut inv = Instance::new("inv0", inv);
+    let mut buf = Instance::new("buf0", buf);
     let mut and = Instance::new("and0", and);
+    let mut delay_chain = Instance::new("delay_chain", delay_chain);
+    let inv_outline = sc_outline(&lib.pdk, &inv);
     let and_outline = sc_outline(&lib.pdk, &and);
     let tap_outline = sc_outline(&lib.pdk, &tap0);
+    let buf_outline = sc_outline(&lib.pdk, &buf);
 
-    and.loc.x = tap_outline.width();
-    tap1.loc.x = tap_outline.width() + and_outline.width();
+    inv.loc.x = tap_outline.width();
+    buf.loc.x = inv.loc.x + inv_outline.width();
+    and.loc.x = buf.loc.x + buf_outline.width();
+    tap1.loc.x = and.loc.x + and_outline.width();
+    delay_chain.loc.y = inv_outline.height();
+    delay_chain.reflect_vert_anchored();
+
+    let mut router = Router::new("sram22_control_logic_route", lib.pdk.clone());
+    let cfg = router.cfg();
+    let m0 = cfg.layerkey(0);
+    let m1 = cfg.layerkey(1);
+    let m2 = cfg.layerkey(2);
+
+    // pc_b to buffer
+    let src = inv.port("y").largest_rect(m0).unwrap();
+    let dst = buf.port("x").largest_rect(m0).unwrap();
+    let mut trace = router.trace(src, 0);
+    trace.place_cursor_centered().vert_to(src.center().y - 225).horiz_to_rect(dst);
+    cell.add_pin("pc_b", m0, trace.rect());
+
+    // buffer to and gate
+    let src = buf.port("a").largest_rect(m0).unwrap();
+    let dst = and.port("a").largest_rect(m0).unwrap();
+    let mut trace = router.trace(src, 0);
+    trace.place_cursor_centered().up().horiz_to_rect(dst).contact_down(dst);
+    cell.add_pin("wl_en", m1, trace.rect());
+    cell.add_pin_from_port(and.port("x").named("write_driver_en"), m0);
+
+    // connect clocks
+    let src = inv.port("a").largest_rect(m0).unwrap();
+    let dst = delay_chain.port("din").largest_rect(m0).unwrap();
+    let mut trace = router.trace(src, 0);
+    trace.place_cursor_centered().up().up().vert_to_rect(dst).contact_down(dst);
+    cell.add_pin("clk", m2, trace.rect());
+    cell.add_pin_from_port(delay_chain.port("dout").named("sense_en"), m0);
 
     cell.layout_mut().add_inst(tap0);
+    cell.layout_mut().add_inst(inv);
+    cell.layout_mut().add_inst(buf);
     cell.layout_mut().add_inst(and);
     cell.layout_mut().add_inst(tap1);
+    cell.layout_mut().add_inst(delay_chain);
+    cell.layout_mut().add_inst(router.finish());
 
     let ptr = Ptr::new(cell);
     lib.lib.cells.push(ptr.clone());
