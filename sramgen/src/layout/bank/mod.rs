f8d84e1269e0ace997163d8a1e9fa20f1436e8e1
Rahul Kumar <rahulkumar@berkeley.edu>
2022-11-10 17:31:16 -0800

    Increase met2 strap blockage around output buffers

--- sramgen/src/layout/bank/mod.rs
+++ sramgen/src/layout/bank/mod.rs
@@ -1275,7 +1275,7 @@ pub fn draw_sram_bank(lib: &mut PdkLib, params: SramBankParams) -> Result<Physic
                     .set_min_width()
                     .horiz_to_rect(src)
                     .down();
-                power_grid.add_padded_blockage(2, dout_trace.rect().expand(300));
+                power_grid.add_padded_blockage(2, dout_trace.rect().expand(500));
 
                 cell.add_pin(
                     format!("dout_{i}"),
