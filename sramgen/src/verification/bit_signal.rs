b9c00ae2b7af3cac52fa91c91f3af33be1130e35
Rahul Kumar <rahulkumar@berkeley.edu>
2022-11-16 23:40:38 -0800

    BitSignal creation methods

--- sramgen/src/verification/bit_signal.rs
+++ sramgen/src/verification/bit_signal.rs
@@ -18,4 +18,36 @@ impl BitSignal {
     pub fn bits(&self) -> impl Iterator<Item = bool> + '_ {
         self.bits.iter().copied()
     }
+
+    pub fn from_u32(mut value: u32, width: usize) -> Self {
+        assert!(width <= 32);
+        let mut bits = Vec::with_capacity(width);
+        for _ in 0..width {
+            bits.push(value & 1 != 0);
+            value >>= 1;
+        }
+        Self { bits }
+    }
+
+    pub fn from_u64(mut value: u64, width: usize) -> Self {
+        assert!(width <= 64);
+        let mut bits = Vec::with_capacity(width);
+        for _ in 0..width {
+            bits.push(value & 1 != 0);
+            value >>= 1;
+        }
+        Self { bits }
+    }
+
+    #[inline]
+    pub fn from_vec(bits: Vec<bool>) -> Self {
+        Self { bits }
+    }
+
+    #[inline]
+    pub fn from_slice(bits: &[bool]) -> Self {
+        Self {
+            bits: bits.to_vec(),
+        }
+    }
 }
