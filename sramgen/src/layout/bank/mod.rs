006a0d8e1d52debd70ff6ef2770dd27d7cb61e27
Rahul Kumar <rahulkumar@berkeley.edu>
2022-11-24 20:31:55 -0800

    More precise met2 jog condition

--- sramgen/src/layout/bank/mod.rs
+++ sramgen/src/layout/bank/mod.rs
@@ -917,7 +917,9 @@ pub fn draw_sram_bank(lib: &mut PdkLib, params: SramBankParams) -> Result<Physic
         .set_width(dst.height())
         .horiz_to(dst.left() - 5 * cfg.line(3));
     power_grid.add_padded_blockage(2, trace.rect().expand(80));
-    if (trace.cursor_rect().center().x - dst.center().x).abs() <= 3 * cfg.line(2) {
+    if (trace.cursor_rect().center().x - dst.center().x).abs()
+        <= 2 * cfg.line(3) + cfg.space(2) + 220
+    {
         // If there isn't enough space to via up to metal 3, jog on metal 2
         trace.set_width(dst.height()).vert_to_rect(dst);
         power_grid.add_padded_blockage(2, trace.rect());
