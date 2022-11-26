7564419d6335503c4af5fbe0825eb8d965223c70
rahulk29 <rahulkumar@berkeley.edu>
2022-11-25 18:57:50 -0800

    Update replica bitcell column generator (#324)
    Generator now can generate dummies and is compatible with the physical
    array structure, which has all wordlines in a row tied together.

--- sramgen/src/tests/rbl.rs
+++ sramgen/src/tests/rbl.rs
@@ -8,8 +8,8 @@ fn test_netlist_replica_bitcell_column() -> Result<(), Box<dyn std::error::Error
     let name = "sramgen_replica_bitcell_column";
     let modules = replica_bitcell_column(ReplicaBitcellColumnParams {
         name: name.to_string(),
-        num_active_cells: 8,
-        height: 16,
+        rows: 64,
+        dummy_rows: 0,
     });
 
     let work_dir = test_work_dir(name);
@@ -23,15 +23,12 @@ fn test_netlist_replica_bitcell_column() -> Result<(), Box<dyn std::error::Error
 }
 
 #[test]
-fn test_netlist_replica_column() -> Result<(), Box<dyn std::error::Error>> {
-    let name = "sramgen_replica_column";
-    let modules = replica_column(ReplicaColumnParams {
+fn test_netlist_replica_bitcell_column_dummies() -> Result<(), Box<dyn std::error::Error>> {
+    let name = "sramgen_replica_column_dummies";
+    let modules = replica_bitcell_column(ReplicaBitcellColumnParams {
         name: name.to_string(),
-        bitcell_params: ReplicaBitcellColumnParams {
-            name: "replica_bitcell_column".to_string(),
-            num_active_cells: 8,
-            height: 16,
-        },
+        rows: 32,
+        dummy_rows: 2,
     });
 
     let work_dir = test_work_dir(name);
