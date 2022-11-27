35b026e3148b15dfcdaa0904ea1a923690f3a625
rohanku <32043201+rohanku@users.noreply.github.com>
2022-11-27 12:24:38 -0800

    create macro for running tests (#332)

--- sramgen/src/tests/sram.rs
+++ sramgen/src/tests/sram.rs
@@ -1,148 +1,59 @@
-use crate::config::{ControlMode, SramConfig};
-
+use crate::config::SramConfig;
+use crate::plan::extract::ExtractionResult;
+use crate::plan::{execute_plan, generate_plan};
+use crate::tests::test_work_dir;
 use crate::Result;
 
-use super::generate_test;
-
-#[test]
-fn test_sram_8x32m2w8_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 32,
-        data_width: 8,
-        mux_ratio: 2,
-        write_size: 8,
-        control: ControlMode::Simple,
-    })
+macro_rules! generate_sram_test {
+    ( $num_words:expr, $data_width:expr, $mux_ratio:expr, $write_size:expr, ControlMode::Simple ) => {
+        paste::paste! {
+            #[test]
+            fn [<test_sram_ $num_words x $data_width m $mux_ratio w $write_size _simple>]() -> Result<()> {
+                crate::tests::sram::test_sram(&crate::config::SramConfig {
+                    num_words: $num_words,
+                    data_width: $data_width,
+                    mux_ratio: $mux_ratio,
+                    write_size: $write_size,
+                    control: crate::config::ControlMode::Simple,
+                })
+            }
+        }
+    };
 }
 
-#[test]
-fn test_sram_16x64m2w16_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 64,
-        data_width: 16,
-        mux_ratio: 2,
-        write_size: 16,
-        control: ControlMode::Simple,
-    })
+pub(crate) use generate_sram_test;
+
+pub(crate) fn test_sram(config: &SramConfig) -> Result<()> {
+    let plan = generate_plan(ExtractionResult {}, config)?;
+    let name = &plan.sram_params.name;
+
+    let work_dir = test_work_dir(name);
+    execute_plan(&work_dir, &plan)?;
+
+    #[cfg(feature = "calibre")]
+    {
+        crate::verification::calibre::run_sram_drc(&work_dir, name)?;
+        crate::verification::calibre::run_sram_lvs(&work_dir, name)?;
+        #[cfg(feature = "pex")]
+        crate::verification::calibre::run_sram_pex(&work_dir, name)?;
+    }
+
+    #[cfg(feature = "spectre")]
+    crate::verification::spectre::run_sram_spectre(&plan.sram_params, &work_dir, name)?;
+
+    Ok(())
 }
 
-#[test]
-fn test_sram_16x64m2w8_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 64,
-        data_width: 16,
-        mux_ratio: 2,
-        write_size: 8,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_16x64m2w4_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 64,
-        data_width: 16,
-        mux_ratio: 2,
-        write_size: 4,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_16x64m2w2_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 64,
-        data_width: 16,
-        mux_ratio: 2,
-        write_size: 2,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_8x128m4w8_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 128,
-        data_width: 8,
-        mux_ratio: 4,
-        write_size: 8,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_8x128m4w2_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 128,
-        data_width: 8,
-        mux_ratio: 4,
-        write_size: 2,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_4x256m8w4_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 256,
-        data_width: 4,
-        mux_ratio: 8,
-        write_size: 4,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_4x256m8w2_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 256,
-        data_width: 4,
-        mux_ratio: 8,
-        write_size: 2,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_32x256m2w32_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 256,
-        data_width: 32,
-        mux_ratio: 2,
-        write_size: 32,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_64x128m2w64_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 128,
-        data_width: 64,
-        mux_ratio: 2,
-        write_size: 64,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_64x128m2w32_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 128,
-        data_width: 64,
-        mux_ratio: 2,
-        write_size: 32,
-        control: ControlMode::Simple,
-    })
-}
-
-#[test]
-fn test_sram_64x128m2w2_simple() -> Result<()> {
-    generate_test(&SramConfig {
-        num_words: 128,
-        data_width: 64,
-        mux_ratio: 2,
-        write_size: 2,
-        control: ControlMode::Simple,
-    })
-}
+generate_sram_test!(32, 8, 2, 8, ControlMode::Simple);
+generate_sram_test!(64, 16, 2, 16, ControlMode::Simple);
+generate_sram_test!(64, 16, 2, 8, ControlMode::Simple);
+generate_sram_test!(64, 16, 2, 4, ControlMode::Simple);
+generate_sram_test!(64, 16, 2, 2, ControlMode::Simple);
+generate_sram_test!(128, 8, 4, 8, ControlMode::Simple);
+generate_sram_test!(128, 8, 4, 2, ControlMode::Simple);
+generate_sram_test!(256, 4, 8, 4, ControlMode::Simple);
+generate_sram_test!(256, 4, 8, 2, ControlMode::Simple);
+generate_sram_test!(256, 32, 2, 32, ControlMode::Simple);
+generate_sram_test!(128, 64, 2, 64, ControlMode::Simple);
+generate_sram_test!(128, 64, 2, 32, ControlMode::Simple);
+generate_sram_test!(128, 64, 2, 2, ControlMode::Simple);
