2a209d3a4bd01a01c094eada1fe7fadf6e409793
rohanku <rohankumar@berkeley.edu>
2022-11-30 10:35:41 -0800

    clean up control layout code

--- sramgen/src/layout/rows.rs
+++ sramgen/src/layout/rows.rs
@@ -1,23 +1,38 @@
-use grid::*;
-use layout21::raw::TransformTrait;
-use layout21::raw::{translate::Translate, Instance, Point};
+use layout21::raw::translate::Translate;
+use layout21::raw::{Instance, Point, TransformTrait};
 use pdkprims::Pdk;
 
 use crate::layout::common::sc_outline;
 
-pub struct GridCells {
-    grid: Grid<Instance>,
+pub struct AlignedRows {
+    rows: Vec<Vec<Instance>>,
+    grow_right: bool,
+    grow_up: bool,
 }
 
-impl GridCells {
+impl AlignedRows {
     #[inline]
     pub fn new() -> Self {
-        Self { grid: grid![] }
+        Self {
+            rows: vec![],
+            grow_right: true,
+            grow_up: true,
+        }
+    }
+
+    pub fn grow_down(&mut self) -> &mut Self {
+        self.grow_up = false;
+        self
+    }
+
+    pub fn grow_left(&mut self) -> &mut Self {
+        self.grow_right = false;
+        self
     }
 
     #[inline]
     pub fn add_row(&mut self, row: Vec<Instance>) {
-        self.grid.push_row(row);
+        self.rows.push(row);
     }
 
     #[inline]
@@ -25,19 +40,29 @@ impl GridCells {
         self.place_inner(pdk)
     }
 
+    pub fn get(&self, row: usize, col: usize) -> &Instance {
+        &self.rows[row][col]
+    }
+
+    pub fn get_mut(&mut self, row: usize, col: usize) -> &mut Instance {
+        &mut self.rows[row][col]
+    }
+
     fn place_inner(&mut self, pdk: &Pdk) {
-        let (rows, cols) = self.grid.size();
         let mut prev_row: Option<Instance> = None;
 
-        for r in 0..rows {
+        for row in self.rows.iter_mut() {
             let mut prev: Option<Instance> = None;
-            for c in 0..cols {
-                let instance = self.grid.get_mut(r, c).unwrap();
+            for (c, instance) in row.iter_mut().enumerate() {
                 if let Some(ref p) = prev {
                     let p_outline_bbox = sc_outline(pdk, p).transform(&p.transform());
                     let outline_bbox = sc_outline(pdk, instance).transform(&instance.transform());
                     instance.translate(Point::new(
-                        p_outline_bbox.p1.x - outline_bbox.p0.x,
+                        if self.grow_right {
+                            p_outline_bbox.p1.x - outline_bbox.p0.x
+                        } else {
+                            p_outline_bbox.p0.x - outline_bbox.p0.x
+                        },
                         p_outline_bbox.p0.y - outline_bbox.p0.y,
                     ));
                 } else if let Some(ref p) = prev_row {
@@ -45,7 +70,11 @@ impl GridCells {
                     let outline_bbox = sc_outline(pdk, instance).transform(&instance.transform());
                     instance.translate(Point::new(
                         p_outline_bbox.p0.x - outline_bbox.p0.x,
-                        p_outline_bbox.p0.y - outline_bbox.p1.y,
+                        if self.grow_up {
+                            p_outline_bbox.p1.y - outline_bbox.p0.y
+                        } else {
+                            p_outline_bbox.p0.y - outline_bbox.p1.y
+                        },
                     ));
                 }
                 prev = Some(instance.clone());
@@ -57,17 +86,17 @@ impl GridCells {
     }
 
     #[inline]
-    pub fn grid(&self) -> &Grid<Instance> {
-        &self.grid
+    pub fn rows(&self) -> &Vec<Vec<Instance>> {
+        &self.rows
     }
 
     #[inline]
     pub fn into_instances(self) -> Vec<Instance> {
-        self.grid.into_vec()
+        self.rows.into_iter().flatten().collect()
     }
 }
 
-impl Default for GridCells {
+impl Default for AlignedRows {
     #[inline]
     fn default() -> Self {
         Self::new()
