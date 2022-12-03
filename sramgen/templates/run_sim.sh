ed210b9bcf1cf0c1e5acc3adee55239dde3765b3
Rahul Kumar <rahulkumar@berkeley.edu>
2022-12-02 16:32:42 -0800

    Fix run script template

--- sramgen/templates/run_sim.sh
+++ sramgen/templates/run_sim.sh
@@ -6,7 +6,7 @@ source /tools/B/rahulkumar/sky130/priv/drc/.bashrc
 
 set -e
 
-spectre -64 +spice + aps -format psfascii \
+spectre -64 +spice +aps -format psfascii \
   {{ spice_path }} \
   -raw {{ raw_output_dir }} \
   =log {{ log_path }}
