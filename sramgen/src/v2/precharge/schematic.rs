2e4715f835eb2048b8bd3f1de1e1ac27403f1ad6
rohanku <rohankumar@berkeley.edu>
2023-01-24 15:08:28 -0800

    work on precharge schematic

--- sramgen/src/v2/precharge/schematic.rs
+++ sramgen/src/v2/precharge/schematic.rs
@@ -1,10 +1,64 @@
+use substrate::schematic::circuit::Direction;
+
 use super::Precharge;
 
 impl Precharge {
     pub(crate) fn schematic(
         &self,
-        _ctx: &mut substrate::schematic::context::SchematicCtx,
+        ctx: &mut substrate::schematic::context::SchematicCtx,
     ) -> substrate::error::Result<()> {
-        Ok(())
+        let length = self.params.length;
+
+        let vdd = ctx.port("vin", Direction::Inout);
+        let bl = ctx.port("bl", Direction::Inout);
+        let br = ctx.port("br", Direction::Inout);
+        let en_b = ctx.port("en_b", Direction::Input);
+
+        let mut m = Module::new(&params.name);
+        m.add_ports_inout(&[&vdd, &bl, &br]);
+        m.add_port_input(&en_b);
+
+        m.add_instance(
+            Mosfet {
+                name: "bl_pull_up".to_string(),
+                width: params.pull_up_width,
+                length,
+                drain: bl.clone(),
+                source: vdd.clone(),
+                gate: en_b.clone(),
+                body: vdd.clone(),
+                mos_type: MosType::Pmos,
+            }
+            .into(),
+        );
+        m.add_instance(
+            Mosfet {
+                name: "br_pull_up".to_string(),
+                width: params.pull_up_width,
+                length,
+                drain: br.clone(),
+                source: vdd.clone(),
+                gate: en_b.clone(),
+                body: vdd.clone(),
+                mos_type: MosType::Pmos,
+            }
+            .into(),
+        );
+
+        m.add_instance(
+            Mosfet {
+                name: "equalizer".to_string(),
+                width: params.equalizer_width,
+                length,
+                drain: bl.clone(),
+                source: br.clone(),
+                gate: en_b.clone(),
+                body: vdd.clone(),
+                mos_type: MosType::Pmos,
+            }
+            .into(),
+        );
+
+        m
     }
 }
