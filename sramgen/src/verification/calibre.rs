06ccfabe5968a4346a9f7b82337519f69ba8422f
rohanku <32043201+rohanku@users.noreply.github.com>
2023-01-31 23:26:19 -0800

    add calibre DRC checks to v2 tests (#374)

--- sramgen/src/verification/calibre.rs
+++ sramgen/src/verification/calibre.rs
@@ -9,8 +9,8 @@ use calibre::pex::{run_pex, PexParams};
 use calibre::RuleCheck;
 use std::path::{Path, PathBuf};
 
-const SKY130_DRC_RULES_PATH: &str = "/tools/B/rahulkumar/sky130/priv/drc/sram_drc_rules";
-const SKY130_LVS_RULES_PATH: &str =
+pub(crate) const SKY130_DRC_RULES_PATH: &str = "/tools/commercial/skywater/swtech130/skywater-src-nda/s8/V2.0.1/DRC/Calibre/s8_drcRules";
+pub(crate) const SKY130_LVS_RULES_PATH: &str =
     "/tools/commercial/skywater/swtech130/skywater-src-nda/s8/V2.0.1/LVS/Calibre/lvs_s8_opts";
 
 #[cfg(feature = "pex")]
