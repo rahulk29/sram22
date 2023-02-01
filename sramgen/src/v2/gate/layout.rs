d190f5e4b95c9024cfa308743f83500614da908b
Rahul Kumar <rahulkumar@berkeley.edu>
2023-02-01 14:48:50 -0800

    Nand3 gate layout generator

--- sramgen/src/v2/gate/layout.rs
+++ sramgen/src/v2/gate/layout.rs
@@ -33,12 +33,8 @@ impl Nand2 {
         ctx: &mut substrate::layout::context::LayoutCtx,
     ) -> substrate::error::Result<()> {
         let db = ctx.mos_db();
-        let nmos = db
-            .query(Query::builder().kind(MosKind::Nmos).build().unwrap())
-            .unwrap();
-        let pmos = db
-            .query(Query::builder().kind(MosKind::Pmos).build().unwrap())
-            .unwrap();
+        let nmos = db.default_nmos().unwrap();
+        let pmos = db.default_pmos().unwrap();
 
         let params = LayoutMosParams {
             skip_sd_metal: vec![vec![1], vec![]],
@@ -64,8 +60,7 @@ impl Nand2 {
         let mos = ctx.instantiate::<LayoutMos>(&params)?;
         ctx.draw_ref(&mos)?;
 
-        let layers = ctx.layers();
-        let m0 = layers.get(Selector::Metal(0))?;
+        let m0 = mos.port("sd_0_0")?.any_layer();
 
         let a = mos.port("sd_0_2")?.largest_rect(m0)?;
         let b = mos.port("sd_1_2")?.largest_rect(m0)?;
@@ -77,7 +72,7 @@ impl Nand2 {
 
         let space = Span::new(a.right(), c.left());
         let vspan = Span::new(a.top(), c.bottom());
-        let hspan = Span::from_center_span_gridded(space.center(), 170, 5);
+        let hspan = Span::from_center_span_gridded(space.center(), a.height(), ctx.pdk().layout_grid());
         ctx.draw_rect(m0, Rect::from_spans(hspan, vspan));
         ctx.draw_rect(
             m0,
@@ -98,8 +93,65 @@ impl Nand2 {
 impl Nand3 {
     pub(crate) fn layout(
         &self,
-        _ctx: &mut substrate::layout::context::LayoutCtx,
+        ctx: &mut substrate::layout::context::LayoutCtx,
     ) -> substrate::error::Result<()> {
+        let db = ctx.mos_db();
+        let nmos = db.default_nmos().unwrap();
+        let pmos = db.default_pmos().unwrap();
+
+        let params = LayoutMosParams {
+            skip_sd_metal: vec![vec![1, 2], vec![]],
+            deep_nwell: true,
+            contact_strategy: GateContactStrategy::SingleSide,
+            devices: vec![
+                MosParams {
+                    w: self.params.nwidth,
+                    l: self.params.length,
+                    m: 1,
+                    nf: 3,
+                    id: nmos.id(),
+                },
+                MosParams {
+                    w: self.params.pwidth,
+                    l: self.params.length,
+                    m: 1,
+                    nf: 3,
+                    id: pmos.id(),
+                },
+            ],
+        };
+        let mos = ctx.instantiate::<LayoutMos>(&params)?;
+        ctx.draw_ref(&mos)?;
+
+        let m0 = mos.port("sd_0_0")?.any_layer();
+
+        let a = mos.port("sd_0_3")?.largest_rect(m0)?;
+        let b = mos.port("sd_1_3")?.largest_rect(m0)?;
+        let c = mos.port("sd_1_1")?.largest_rect(m0)?;
+
+        let out = a.bbox().union(b.bbox()).into_rect();
+        ctx.add_port(CellPort::with_shape("y", m0, out));
+        ctx.draw_rect(m0, out);
+
+        let space = Span::new(a.right(), c.left());
+        let vspan = Span::new(a.top(), c.bottom());
+        let hspan = Span::from_center_span_gridded(space.center(), a.height(), ctx.pdk().layout_grid());
+        ctx.draw_rect(m0, Rect::from_spans(hspan, vspan));
+        ctx.draw_rect(
+            m0,
+            Rect::from_spans(Span::new(hspan.start(), c.right()), c.vspan()),
+        );
+
+        ctx.add_port(mos.port("gate_0")?.into_cell_port().named("a"));
+        ctx.add_port(mos.port("gate_1")?.into_cell_port().named("b"));
+        ctx.add_port(mos.port("gate_2")?.into_cell_port().named("c"));
+        ctx.add_port(mos.port("sd_0_0")?.into_cell_port().named("vss"));
+        let mut vdd_port = mos.port("sd_1_0")?.into_cell_port().named("vdd");
+        vdd_port.merge(mos.port("sd_1_2")?);
+        ctx.add_port(vdd_port);
+
+        ctx.flatten();
+
         Ok(())
     }
 }
