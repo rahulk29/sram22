fbc89bbe088c61947f9a5b0ce730aac63990aec7
rohanku <rohankumar@berkeley.edu>
2023-02-06 10:00:30 -0800

    work on column peripherals schematic

--- sramgen/src/v2/columns/schematic.rs
+++ sramgen/src/v2/columns/schematic.rs
@@ -24,16 +24,22 @@ impl ColPeripherals {
         let clk = ctx.port("clk", Direction::Input);
         let vdd = ctx.port("vdd", Direction::InOut);
         let vss = ctx.port("vss", Direction::InOut);
-        let bl = ctx.bus_port("bl", self.params.cols, Direction::InOut);
-        let br = ctx.bus_port("br", self.params.cols, Direction::InOut);
+        let bl = ctx.bus_port("bl", cols, Direction::InOut);
+        let br = ctx.bus_port("br", cols, Direction::InOut);
         let bl_dummy = ctx.bus_port("bl_dummy", 2, Direction::InOut);
         let br_dummy = ctx.bus_port("br_dummy", 2, Direction::InOut);
-        let en_b = ctx.port("en_b", Direction::Input);
-        let sel_b = ctx.bus_port("sel_b", self.params.cols, Direction::Input);
+        let pc_b = ctx.port("pc_b", Direction::Input);
+        let sel_b = ctx.bus_port("sel_b", cols, Direction::Input);
+        let we = ctx.port("we", Direction::Input);
         let wmask = ctx.port("wmask", Direction::Input);
-        let data = ctx.bus_port("data", word_length, Direction::Input);
+        let data_in = ctx.bus_port("data", word_length, Direction::Input);
+        let data_out = ctx.bus_port("outp", word_length, Direction::Output);
 
-        let pc = ctx.instantiate::<Precharge>(&self.params.pc)?;
+        for i in 0..word_length {
+            let col = ctx.instantiate::<Column>(&self.params)?;
+            col.connect_all([("clk", &clk), ("vdd", &vdd), ("vss", &vss)]);
+        }
+
         Ok(())
     }
 }
@@ -45,18 +51,18 @@ impl Column {
         let vss = ctx.port("vss", Direction::InOut);
         let bl = ctx.bus_port("bl", self.params.rmux.mux_ratio, Direction::InOut);
         let br = ctx.bus_port("br", self.params.rmux.mux_ratio, Direction::InOut);
-        let en_b = ctx.port("en_b", Direction::Input);
+        let pc_b = ctx.port("pc_b", Direction::Input);
         let sel_b = ctx.bus_port("sel_b", self.params.rmux.mux_ratio, Direction::Input);
         let we = ctx.port("we", Direction::Input);
         let wmask = ctx.port("wmask", Direction::Input);
-        let data = ctx.port("data", Direction::Input);
-        let outp = ctx.port("outp", Direction::Output);
-        let outn = ctx.port("outn", Direction::Output);
+        let data_in = ctx.port("data_in", Direction::Input);
+        let data_out = ctx.port("data_out", Direction::Output);
 
         let bl_out = ctx.signal("bl_out");
         let br_out = ctx.signal("br_out");
         let sa_outp = ctx.signal("sa_outp");
         let sa_outn = ctx.signal("sa_outn");
+        let diff_buf_outn = ctx.port("diff_buf_outn", Direction::Output);
         let q = ctx.signal("q");
         let q_b = ctx.signal("q_b");
 
@@ -68,7 +74,7 @@ impl Column {
             let br_i = br.index(i);
 
             let mut pc_i = pc.clone();
-            pc_i.connect_all([("vdd", &vdd), ("bl", &bl_i), ("br", &br_i), ("en_b", &en_b)]);
+            pc_i.connect_all([("vdd", &vdd), ("bl", &bl_i), ("br", &br_i), ("en_b", &pc_b)]);
             pc_i.set_name(format!("precharge_{i}"));
             ctx.add_instance(pc_i);
 
@@ -123,8 +129,8 @@ impl Column {
             ("vss", &vss),
             ("din1", &sa_outp),
             ("din2", &sa_outn),
-            ("dout1", &outp),
-            ("dout2", &outn),
+            ("dout1", &data_out),
+            ("dout2", &diff_buf_outn),
         ]);
         buf.set_name("buf");
         ctx.add_instance(buf);
@@ -134,7 +140,7 @@ impl Column {
             ("vdd", &vdd),
             ("gnd", &vss),
             ("clk", &clk),
-            ("d", &data),
+            ("d", &data_in),
             ("q", &q),
             ("q_n", &q_b),
         ]);
