8c8e8b2ba175b6349d9c19457df6ccfee37f3ea3
rohanku <rohankumar@berkeley.edu>
2023-02-22 17:26:53 -0800

    merge

--- sramgen/src/v2/columns/mod.rs
+++ sramgen/src/v2/columns/mod.rs
@@ -53,9 +53,10 @@ impl Component for ColPeripherals {
         _ctx: &substrate::data::SubstrateCtx,
     ) -> substrate::error::Result<Self> {
         if params.rmux.mux_ratio != params.wmux.mux_ratio {
-            return Err(substrate::error::SubstrateError::Component(
+            return Err(substrate::error::ErrorSource::Component(
                 substrate::component::error::Error::InvalidParams,
-            ));
+            )
+            .into());
         }
         Ok(Self {
             params: params.clone(),
