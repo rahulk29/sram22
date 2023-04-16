0678299a5595c3542e43d8ea23b0789bafb69bd2
Rahul Kumar <rahulkumar@berkeley.edu>
2023-04-15 17:43:59 -0700

    Makefile updates, zero one pattern generator testbench

--- sv/tb/zero_one_patgen_tb.sv
+++ sv/tb/zero_one_patgen_tb.sv
@@ -1,5 +1,8 @@
 
 module zero_one_patgen_tb;
+  localparam int MaxAddr = 31;
+  localparam int AddrWidth = $clog2(MaxAddr);
+  localparam int DataWidth = 8;
   bit clk;
   always #10 clk = ~clk;
   initial begin
@@ -7,8 +10,8 @@ module zero_one_patgen_tb;
   end
 
   det_patgen_if #(
-      .MAX_ADDR  (127),
-      .DATA_WIDTH(8),
+      .MAX_ADDR  (MaxAddr),
+      .DATA_WIDTH(DataWidth),
       .MASK_WIDTH(2)
   ) if0 (
       .clk
@@ -20,10 +23,51 @@ module zero_one_patgen_tb;
     $dumpvars;
     if0.rst = 1;
     repeat (16) @(posedge if0.clk);
+    #1;
     if0.rst = 0;
     if0.en  = 1;
+
+    // Write 0
+    for (int i = 0; i <= MaxAddr; i++) begin
+      @(posedge if0.clk);
+      assert (if0.addr == AddrWidth'(i))
+      else $error("Wrong address: expected %d, got %d at time %0t", i, if0.addr, $time);
+      assert (if0.data == {DataWidth{1'b0}});
+      assert (if0.we);
+      assert (!if0.re);
+    end
+
+    // Read 0
+    for (int i = 0; i <= MaxAddr; i++) begin
+      @(posedge if0.clk);
+      assert (if0.addr == AddrWidth'(i));
+      assert (if0.check == {DataWidth{1'b0}});
+      assert (!if0.we);
+      assert (if0.re);
+    end
+
+    // Write 1
+    for (int i = 0; i <= MaxAddr; i++) begin
+      @(posedge if0.clk);
+      assert (if0.addr == AddrWidth'(i));
+      assert (if0.data == {DataWidth{1'b1}});
+      assert (if0.we);
+      assert (!if0.re);
+    end
+
+    // Read 1
+    for (int i = 0; i <= MaxAddr; i++) begin
+      @(posedge if0.clk);
+      assert (if0.addr == AddrWidth'(i));
+      assert (if0.check == {DataWidth{1'b1}});
+      assert (!if0.we);
+      assert (if0.re);
+    end
+
+
     repeat (532) @(posedge if0.clk);
     assert (if0.done);
+    $display("Test passed.");
     $finish;
   end
 endmodule
