9a9de8796f019f9b789637ff41365be646d871e2
rohanku <rohankumar@berkeley.edu>
2023-05-05 22:53:11 -0700

    add back vias to metal 2

--- sramgen/src/v2/bitcell_array/mod.rs
+++ sramgen/src/v2/bitcell_array/mod.rs
@@ -196,6 +196,13 @@ mod tests {
                                             .with(!dir, r.span(!dir))
                                             .build();
                                         ctx.draw_rect(port_metal, rect);
+                                        let viap = ViaParams::builder()
+                                        .layers(ring_metal, port_metal)
+                                        .geometry(ring_rect, rect)
+                                        .expand(substrate::layout::elements::via::ViaExpansion::LongerDirection)
+                                        .build();
+                                        let via = ctx.instantiate::<Via>(&viap)?;
+                                        ctx.draw(via)?;
                                     }
                                     Dir::Vert => match ring_port_name {
                                         "ring_vss" => {
