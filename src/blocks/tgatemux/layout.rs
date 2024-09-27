54b8282318066e3554dd506aaba7896a78f73213
Rahul Kumar <rahulkumar@berkeley.edu>
2024-09-26 21:59:51 -0700

    tgate mux layout (#459)
    * tgate mux layout (wip)
    
    * add block diagram, work on tgate mux, more pc_b buffers

--- src/blocks/tgatemux/layout.rs
+++ src/blocks/tgatemux/layout.rs
@@ -1,3 +1,4 @@
+use std::fs::metadata;
 use subgeom::bbox::BoundBox;
 use subgeom::orientation::Named;
 use subgeom::{Dir, Point, Rect, Side, Span};
@@ -20,10 +21,10 @@ use substrate::pdk::mos::{GateContactStrategy, LayoutMosParams, MosParams};
 use super::{TGateMux, TGateMuxCent, TGateMuxEnd, TGateMuxParams};
 
 use derive_builder::Builder;
+use substrate::layout::placement::align::AlignRect;
 
 const GATE_LINE: i64 = 320;
 const GATE_SPACE: i64 = 180;
-const POWER_VSPAN: Span = Span::new_unchecked(3_000, 3_800);
 
 impl TGateMux {
     pub(crate) fn layout(
@@ -35,10 +36,13 @@ impl TGateMux {
             .run_script::<crate::blocks::precharge::layout::PhysicalDesignScript>(&NoParams)?;
 
         let db = ctx.mos_db();
-        let mos = db
+        let pmos = db
             .query(Query::builder().kind(MosKind::Pmos).build().unwrap())
             .unwrap();
-        let params = LayoutMosParams {
+        let nmos = db
+            .query(Query::builder().kind(MosKind::Nmos).build().unwrap())
+            .unwrap();
+        let pmos_params = LayoutMosParams {
             skip_sd_metal: vec![vec![]; 3],
             deep_nwell: true,
             contact_strategy: GateContactStrategy::SingleSide,
@@ -48,26 +52,53 @@ impl TGateMux {
                     l: self.params.length,
                     m: 1,
                     nf: 1,
-                    id: mos.id(),
+                    id: pmos.id(),
                 },
                 MosParams {
                     w: self.params.pwidth,
                     l: self.params.length,
                     m: 1,
                     nf: 1,
-                    id: mos.id(),
+                    id: pmos.id(),
+                },
+            ],
+        };
+        let nmos_params = LayoutMosParams {
+            skip_sd_metal: vec![vec![]; 3],
+            deep_nwell: true,
+            contact_strategy: GateContactStrategy::SingleSide,
+            devices: vec![
+                MosParams {
+                    w: self.params.nwidth,
+                    l: self.params.length,
+                    m: 1,
+                    nf: 1,
+                    id: nmos.id(),
+                },
+                MosParams {
+                    w: self.params.nwidth,
+                    l: self.params.length,
+                    m: 1,
+                    nf: 1,
+                    id: nmos.id(),
                 },
             ],
         };
 
-        let mut mos = ctx.instantiate::<LayoutMos>(&params)?;
-        mos.set_orientation(Named::R90);
+        let mut pmos = ctx.instantiate::<LayoutMos>(&pmos_params)?;
+        pmos.set_orientation(Named::R90);
+        let mut nmos = ctx.instantiate::<LayoutMos>(&nmos_params)?;
+        nmos.set_orientation(Named::R270);
 
-        mos.place_center(Point::new(pc.width / 2, mos.bbox().height() / 2));
-        ctx.draw_ref(&mos)?;
+        pmos.place_center(Point::new(pc.width / 2, pmos.bbox().height() / 2));
+        nmos.align_centers_horizontally(&pmos);
+        nmos.align_above(&pmos, 400);
+
+        ctx.draw_ref(&pmos)?;
+        ctx.draw_ref(&nmos)?;
 
         // Below this, routing follows `in_tracks`.
-        let in_top = mos.brect().top() - 600;
+        let in_top = nmos.brect().top() - 600;
 
         let jog = SimpleJog::builder()
             .dir(Dir::Vert)
@@ -84,39 +115,21 @@ impl TGateMux {
         let out_top = jog.dst_pos();
         ctx.draw(jog)?;
 
-        // Below this, routing follows `out_tracks`.
+        // The lower extent of BL/BR routing.
         let out_bot = 600;
-
-        let jog = SimpleJog::builder()
-            .dir(Dir::Vert)
-            .src_pos(out_bot)
-            .src([pc.out_tracks.index(0), pc.out_tracks.index(2)])
-            .dst([pc.in_tracks.index(1), pc.in_tracks.index(2)])
-            .line(pc.v_line)
-            .space(pc.v_space)
-            .layer(pc.v_metal)
-            .build()
-            .unwrap();
-
-        // Above this, routing follows `in_tracks`.
-        let in_bot = jog.dst_pos();
-        ctx.draw(jog)?;
 
         let stripe_hspan = Span::new(-pc.width, 2 * pc.width);
         let abs_bot = -(GATE_LINE + GATE_SPACE) * self.params.mux_ratio as i64;
+        let abs_top = nmos.brect().top() + (GATE_LINE + GATE_SPACE) * self.params.mux_ratio as i64;
 
         for i in [0, 2] {
-            let top = mos.brect().top();
-            let rect = Rect::from_spans(pc.out_tracks.index(i), Span::new(out_top, top));
-            ctx.draw_rect(pc.v_metal, rect);
-
-            let rect = Rect::from_spans(pc.out_tracks.index(i), Span::new(abs_bot, out_bot));
+            let rect = Rect::from_spans(pc.out_tracks.index(i), Span::new(out_top, abs_top));
             ctx.draw_rect(pc.v_metal, rect);
         }
 
         let mut tracks = Vec::with_capacity(pc.in_tracks.len());
         for i in 0..pc.in_tracks.len() {
-            let rect = Rect::from_spans(pc.in_tracks.index(i), Span::new(in_bot, in_top));
+            let rect = Rect::from_spans(pc.in_tracks.index(i), Span::new(out_bot, in_top));
             ctx.draw_rect(pc.v_metal, rect);
             tracks.push(rect);
         }
@@ -125,9 +138,19 @@ impl TGateMux {
         ctx.add_port(CellPort::with_shape("br", pc.v_metal, tracks[2]))?;
 
         let mut metadata = Metadata::builder();
+        metadata.sel_tracks_ystart(nmos.brect().top());
 
+        for (port, idx) in [("sd_1_1", 2), ("sd_0_0", 1)] {
+            let target = nmos.port(port)?.largest_rect(pc.m0)?;
+            let viap = ViaParams::builder()
+                .layers(pc.m0, pc.v_metal)
+                .geometry(tracks[idx], target)
+                .build();
+            let via = ctx.instantiate::<Via>(&viap)?;
+            ctx.draw(via)?;
+        }
         for (port, idx) in [("sd_1_1", 1), ("sd_0_0", 2)] {
-            let target = mos.port(port)?.largest_rect(pc.m0)?;
+            let target = pmos.port(port)?.largest_rect(pc.m0)?;
             let viap = ViaParams::builder()
                 .layers(pc.m0, pc.v_metal)
                 .geometry(tracks[idx], target)
@@ -136,11 +159,17 @@ impl TGateMux {
             ctx.draw(via)?;
         }
 
-        for (port, idx, x, side, name) in [
-            ("sd_1_0", 3, pc.width, Side::Right, "bl_out"),
-            ("sd_0_1", 0, 0, Side::Left, "br_out"),
+        let mut split_via0 = Vec::new();
+        let mut split_via1 = Vec::new();
+        let mut blbr_stripes = Vec::new();
+
+        for (inst, port, idx, x, side, name) in [
+            (&pmos, "sd_1_0", 3, pc.width, Side::Right, Some("bl_out")),
+            (&pmos, "sd_0_1", 0, 0, Side::Left, Some("br_out")),
+            (&nmos, "sd_0_1", 3, pc.width, Side::Right, None),
+            (&nmos, "sd_1_0", 0, 0, Side::Left, None),
         ] {
-            let target = mos.port(port)?.largest_rect(pc.m0)?;
+            let target = inst.port(port)?.largest_rect(pc.m0)?;
             let viap = ViaParams::builder()
                 .layers(pc.m0, pc.v_metal)
                 .geometry(
@@ -149,7 +178,9 @@ impl TGateMux {
                 )
                 .build();
             let via = ctx.instantiate::<Via>(&viap)?;
-            metadata.split_via0(viap);
+            if side == Side::Left {
+                split_via0.push(viap);
+            }
             ctx.draw_rect(
                 pc.m0,
                 Rect::from_spans(
@@ -170,61 +201,84 @@ impl TGateMux {
                 .expand(ViaExpansion::LongerDirection)
                 .build();
             let via = ctx.instantiate::<Via>(&viap)?;
-            metadata.split_via1(viap);
-            metadata.bot_stripe(stripe.vspan());
+            if side == Side::Left {
+                split_via1.push(viap);
+            }
+            blbr_stripes.push(stripe.vspan());
             ctx.draw_ref(&via)?;
 
             ctx.draw_rect(pc.h_metal, stripe);
-            ctx.add_port(CellPort::with_shape(name, pc.h_metal, stripe))
-                .unwrap();
+            if let Some(name) = name {
+                ctx.add_port(CellPort::with_shape(name, pc.h_metal, stripe))
+                    .unwrap();
+            }
         }
 
+        let vdd_stripe = Span::new(blbr_stripes[1].stop(), blbr_stripes[0].start()).shrink_all(300);
+        let vss_stripe = Span::new(blbr_stripes[3].stop(), blbr_stripes[2].start()).shrink_all(300);
+        if vss_stripe.length() < 200 || vdd_stripe.length() < 200 {
+            panic!("insufficient space for vdd/vss stripe");
+        }
+
+        metadata.vdd_stripe(vdd_stripe);
+        metadata.vss_stripe(vss_stripe);
+        metadata.blbr_stripes(blbr_stripes);
+        metadata.split_via0(split_via0);
+        metadata.split_via1(split_via1);
         metadata.split_track(tracks[0]);
+        metadata.nwell_vspan(pmos.brect().vspan());
 
         ctx.set_metadata(metadata.build().unwrap());
 
         assert!(self.params.idx < self.params.mux_ratio);
 
-        for i in 0..self.params.mux_ratio {
-            let vspan = Span::with_stop_and_length(-(GATE_LINE + GATE_SPACE) * i as i64, GATE_LINE);
-            let rect = Rect::from_spans(stripe_hspan, vspan);
-            ctx.draw_rect(pc.h_metal, rect);
-            ctx.add_port(CellPort::with_shape(
-                PortId::new("sel_b", i),
-                pc.h_metal,
-                rect,
-            ))?;
-
-            if i == self.params.idx {
-                let target = mos.port("gate_0")?.largest_rect(pc.m0)?;
-                let gate_conn =
-                    Rect::from_spans(target.hspan(), target.vspan().union(rect.vspan()));
-
-                let viap = ViaParams::builder()
-                    .layers(pc.v_metal, pc.h_metal)
-                    .geometry(Rect::from_spans(pc.out_tracks.index(1), rect.vspan()), rect)
-                    .build();
-                let mut via = ctx.instantiate::<Via>(&viap)?;
-                via.place_center(rect.center());
-                ctx.draw_ref(&via)?;
-
-                let viap = ViaParams::builder()
-                    .layers(pc.m0, pc.v_metal)
-                    .geometry(
-                        gate_conn,
-                        Rect::from_spans(pc.out_tracks.index(1), rect.vspan()),
+        for (inst, port) in [(&pmos, "sel_b"), (&nmos, "sel")] {
+            for i in 0..self.params.mux_ratio {
+                let vspan = if port == "sel" {
+                    Span::with_start_and_length(
+                        nmos.brect().top() + (GATE_LINE + GATE_SPACE) * i as i64,
+                        GATE_LINE,
                     )
-                    .build();
-                let mut via = ctx.instantiate::<Via>(&viap)?;
-                via.place_center(rect.center());
-                ctx.draw_ref(&via)?;
+                } else {
+                    Span::with_stop_and_length(-(GATE_LINE + GATE_SPACE) * i as i64, GATE_LINE)
+                };
+                let rect = Rect::from_spans(stripe_hspan, vspan);
+                ctx.draw_rect(pc.h_metal, rect);
+                ctx.add_port(CellPort::with_shape(PortId::new(port, i), pc.h_metal, rect))?;
 
-                ctx.draw_rect(pc.m0, gate_conn);
+                if i == self.params.idx {
+                    let target = inst.port("gate_0")?.largest_rect(pc.m0)?;
+                    let gate_conn =
+                        Rect::from_spans(target.hspan(), target.vspan().union(rect.vspan()));
+
+                    let viap = ViaParams::builder()
+                        .layers(pc.v_metal, pc.h_metal)
+                        .geometry(Rect::from_spans(pc.out_tracks.index(1), rect.vspan()), rect)
+                        .build();
+                    let mut via = ctx.instantiate::<Via>(&viap)?;
+                    via.place_center(rect.center());
+                    ctx.draw_ref(&via)?;
+
+                    let viap = ViaParams::builder()
+                        .layers(pc.m0, pc.v_metal)
+                        .geometry(
+                            gate_conn,
+                            Rect::from_spans(pc.out_tracks.index(1), rect.vspan()),
+                        )
+                        .build();
+                    let mut via = ctx.instantiate::<Via>(&viap)?;
+                    via.place_center(rect.center());
+                    ctx.draw_ref(&via)?;
+
+                    ctx.draw_rect(pc.m0, gate_conn);
+                }
             }
         }
 
-        let power_stripe = Rect::from_spans(stripe_hspan, POWER_VSPAN);
+        let power_stripe = Rect::from_spans(stripe_hspan, vdd_stripe);
         ctx.draw_rect(pc.h_metal, power_stripe);
+        let ground_stripe = Rect::from_spans(stripe_hspan, vss_stripe);
+        ctx.draw_rect(pc.h_metal, ground_stripe);
 
         let bounds = Rect::from_spans(Span::new(0, pc.width), ctx.brect().vspan());
         ctx.flatten();
@@ -232,7 +286,10 @@ impl TGateMux {
 
         let layers = ctx.layers();
         let nwell = layers.get(Selector::Name("nwell"))?;
-        ctx.draw_rect(nwell, bounds);
+        ctx.draw_rect(
+            nwell,
+            Rect::from_spans(bounds.hspan(), pmos.bbox().into_rect().vspan()),
+        );
 
         let psdm = layers.get(Selector::Name("psdm"))?;
         let implants = ctx
@@ -250,10 +307,14 @@ impl TGateMux {
 
 #[derive(Debug, Builder)]
 struct Metadata {
-    split_via1: ViaParams,
-    split_via0: ViaParams,
+    split_via1: Vec<ViaParams>,
+    split_via0: Vec<ViaParams>,
     split_track: Rect,
-    bot_stripe: Span,
+    blbr_stripes: Vec<Span>,
+    nwell_vspan: Span,
+    sel_tracks_ystart: i64,
+    vdd_stripe: Span,
+    vss_stripe: Span,
 }
 
 impl Metadata {
@@ -291,17 +352,10 @@ fn tgate_mux_tap_layout(
 
     let meta = mux.cell().get_metadata::<Metadata>();
 
-    let mut via1 = ctx.instantiate::<Via>(&meta.split_via1)?;
-    let mut via0 = ctx.instantiate::<Via>(&meta.split_via0)?;
-    via1.place_center(Point::new(width, via1.brect().center().y));
-    via0.place_center(Point::new(width, via0.brect().center().y));
-    ctx.draw_ref(&via1)?;
-    ctx.draw_ref(&via0)?;
-    if !end {
-        via1.place_center(Point::new(0, via1.brect().center().y));
-        via0.place_center(Point::new(0, via0.brect().center().y));
-        ctx.draw_ref(&via1)?;
-        ctx.draw_ref(&via0)?;
+    for via in meta.split_via0.iter().chain(meta.split_via1.iter()) {
+        let mut via = ctx.instantiate::<Via>(via)?;
+        via.place_center(Point::new(width, via.brect().center().y));
+        ctx.draw_ref(&via)?;
     }
 
     let mut vtrack = meta.split_track.double(Side::Left);
@@ -321,17 +375,35 @@ fn tgate_mux_tap_layout(
             rect,
         ))?;
     }
+    for i in 0..params.mux_ratio {
+        let vspan = Span::with_start_and_length(
+            meta.sel_tracks_ystart + (GATE_LINE + GATE_SPACE) * i as i64,
+            GATE_LINE,
+        );
+        let rect = Rect::from_spans(stripe_hspan, vspan);
+        ctx.draw_rect(pc.h_metal, rect);
+        ctx.add_port(CellPort::with_shape(
+            PortId::new("sel", i),
+            pc.h_metal,
+            rect,
+        ))?;
+    }
 
-    let power_stripe = Rect::from_spans(stripe_hspan, POWER_VSPAN);
+    let power_stripe = Rect::from_spans(stripe_hspan, meta.vdd_stripe);
     ctx.draw_rect(pc.h_metal, power_stripe);
     ctx.add_port(CellPort::with_shape("vdd", pc.h_metal, power_stripe))
+        .unwrap();
+    let ground_stripe = Rect::from_spans(stripe_hspan, meta.vss_stripe);
+    ctx.draw_rect(pc.h_metal, ground_stripe);
+    ctx.add_port(CellPort::with_shape("vss", pc.h_metal, ground_stripe))
         .unwrap();
 
     let bounds = Rect::from_spans(Span::new(0, width), mux.brect().vspan());
     ctx.flatten();
     ctx.trim(&bounds);
 
-    let tap_rect = bounds.shrink(300);
+    let ntap_bounds = Rect::from_spans(bounds.hspan(), meta.nwell_vspan);
+    let tap_rect = ntap_bounds.shrink(300);
 
     let layers = ctx.layers();
     let tap = layers.get(Selector::Name("tap"))?;
@@ -361,19 +433,18 @@ fn tgate_mux_tap_layout(
     let via = ctx.instantiate::<Via>(&viap)?;
     ctx.draw_ref(&via)?;
 
-    if !end {
+    for &stripe in meta.blbr_stripes.iter() {
+        if !end {
+            ctx.draw_rect(pc.h_metal, Rect::from_spans(Span::new(0, 200), stripe));
+        }
         ctx.draw_rect(
             pc.h_metal,
-            Rect::from_spans(Span::new(0, 200), meta.bot_stripe),
+            Rect::from_spans(Span::with_stop_and_length(width, 200), stripe),
         );
     }
-    ctx.draw_rect(
-        pc.h_metal,
-        Rect::from_spans(Span::with_stop_and_length(width, 200), meta.bot_stripe),
-    );
 
-    ctx.draw_rect(nwell, bounds);
-    ctx.draw_rect(nsdm, bounds);
+    ctx.draw_rect(nwell, ntap_bounds);
+    ctx.draw_rect(nsdm, ntap_bounds);
 
     Ok(())
 }
