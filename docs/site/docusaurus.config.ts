399edf7dcdfdf4f0efa93710760ffb5d41f564c5
Rahul Kumar <rahulkumar@berkeley.edu>
2024-10-07 10:20:57 -0700

    docusaurus ci

--- docs/site/docusaurus.config.ts
+++ docs/site/docusaurus.config.ts
@@ -8,15 +8,17 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://rahulk29.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sram22/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rahulk29', // Usually your GitHub org/user name.
+  projectName: 'sram22', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
