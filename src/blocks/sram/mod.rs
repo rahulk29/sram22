458fd203afe08b1c6b31052f8a8a4a38b80d0baa
Rahul Kumar <rahulkumar@berkeley.edu>
2024-11-07 11:36:28 -0800

    run libs in parallel

--- src/blocks/sram/mod.rs
+++ src/blocks/sram/mod.rs
@@ -1093,49 +1093,57 @@ pub(crate) mod tests {
                     .expect("failed to write abstract");
                     println!("{}: done writing abstract", stringify!($name));
 
-                    // let timing_spice_path = out_spice(&work_dir, "timing_schematic");
-                    // ctx.write_schematic_to_file_for_purpose::<Sram>(
-                    //     &$params,
-                    //     &timing_spice_path,
-                    //     NetlistPurpose::Timing,
-                    // )
-                    // .expect("failed to write timing schematic");
+                    let timing_spice_path = out_spice(&work_dir, "timing_schematic");
+                    ctx.write_schematic_to_file_for_purpose::<Sram>(
+                        &$params,
+                        &timing_spice_path,
+                        NetlistPurpose::Timing,
+                    )
+                    .expect("failed to write timing schematic");
 
-                    // let sram = ctx.instantiate_layout::<Sram>(&$params).expect("failed to generate layout");
-                    // let brect = sram.brect();
-                    // let width = Decimal::new(brect.width(), 3);
-                    // let height = Decimal::new(brect.height(), 3);
-                    // for (corner, temp, vdd) in [("tt", 25, dec!(1.8)), ("ss", 100, dec!(1.6)), ("ff", 40, dec!(1.95))] {
-                    //     let suffix = match corner {
-                    //         "tt" => "tt_025C_1v80",
-                    //         "ss" => "ss_100C_1v60",
-                    //         "ff" => "ff_n40C_1v95",
-                    //         _ => unreachable!(),
-                    //     };
-                    //     let name = format!("{}_{}", $params.name(), suffix);
-                    //     let params = liberate_mx::LibParams::builder()
-                    //         .work_dir(work_dir.join(format!("lib/{suffix}")))
-                    //         .output_file(crate::paths::out_lib(&work_dir, &name))
-                    //         .corner(corner)
-                    //         .width(width)
-                    //         .height(height)
-                    //         .user_verilog(verilog_path.clone())
-                    //         .cell_name(&*$params.name())
-                    //         .num_words($params.num_words())
-                    //         .data_width($params.data_width())
-                    //         .addr_width($params.addr_width())
-                    //         .wmask_width($params.wmask_width())
-                    //         .mux_ratio($params.mux_ratio())
-                    //         .has_wmask(true)
-                    //         // .source_paths(vec![pex_netlist_path.clone()])
-                    //         .source_paths(vec![timing_spice_path.clone()])
-                    //         .vdd(vdd)
-                    //         .temp(temp)
-                    //         .build()
-                    //         .unwrap();
-                    //     crate::liberate::generate_sram_lib(&params).expect("failed to write lib");
-                    //     println!("{}: done generating LIB for corner `{}`", stringify!($name), corner);
-                    // }
+                    let mut handles = Vec::new();
+                    let sram = ctx.instantiate_layout::<Sram>(&$params).expect("failed to generate layout");
+                    let brect = sram.brect();
+                    let width = Decimal::new(brect.width(), 3);
+                    let height = Decimal::new(brect.height(), 3);
+                    for (corner, temp, vdd) in [("tt", 25, dec!(1.8)), ("ss", 100, dec!(1.6)), ("ff", 40, dec!(1.95))] {
+                        let timing_spice_path = timing_spice_path.clone();
+                        let verilog_path = verilog_path.clone();
+                        let work_dir = work_dir.clone();
+                        handles.push(std::thread::spawn(move || {
+                            let suffix = match corner {
+                                "tt" => "tt_025C_1v80",
+                                "ss" => "ss_100C_1v60",
+                                "ff" => "ff_n40C_1v95",
+                                _ => unreachable!(),
+                            };
+                            let name = format!("{}_{}", $params.name(), suffix);
+                            let params = liberate_mx::LibParams::builder()
+                                .work_dir(work_dir.join(format!("lib/{suffix}")))
+                                .output_file(crate::paths::out_lib(&work_dir, &name))
+                                .corner(corner)
+                                .width(width)
+                                .height(height)
+                                .user_verilog(verilog_path)
+                                .cell_name(&*$params.name())
+                                .num_words($params.num_words())
+                                .data_width($params.data_width())
+                                .addr_width($params.addr_width())
+                                .wmask_width($params.wmask_width())
+                                .mux_ratio($params.mux_ratio())
+                                .has_wmask(true)
+                                // .source_paths(vec![pex_netlist_path.clone()])
+                                .source_paths(vec![timing_spice_path])
+                                .vdd(vdd)
+                                .temp(temp)
+                                .build()
+                                .unwrap();
+                            crate::liberate::generate_sram_lib(&params).expect("failed to write lib");
+                            println!("{}: done generating LIB for corner `{}`", stringify!($name), corner);
+                        }));
+                    }
+                    let handles: Vec<_> = handles.into_iter().map(|handle| handle.join()).collect();
+                    handles.into_iter().collect::<Result<Vec<_>, _>>().expect("failed to join threads");
                 }
 
                 println!("{}: all tasks complete", stringify!($name));
